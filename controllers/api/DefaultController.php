a121cdbd3a0aa56255db766192a265c3b644ed7a
ozgesogukpinar <sogukpnar.ozge@gmail.com>
2018-12-11 14:29:42 +0300

    first initial

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\takvim\controllers\api;
+
+
+/**
+ * Default controller for the `takvim` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\takvim\controllers\frontend;
+
+
+/**
+ * Default controller for the `takvim` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
